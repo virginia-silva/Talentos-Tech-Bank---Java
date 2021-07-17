c9e4801a719e1efabae2df04132c512beb84c10a
Virginia <virginia_hol@hotmail.com>
2021-07-17 16:21:51 -0300

    Correcao de erros

---
+++ src/Aula01/Main.java
@@ -0,0 +1,8 @@
+package Aula01;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Olá Talentos Tech Bank");
+        System.out.println(System.getProperty("java.version"));
+    }
+}
